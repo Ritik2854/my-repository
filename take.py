d94f2108e655f90588ef80f418bf9bcbd45bcfed
Ritik2854 <ritikchauhan2854@gmail.com>
2026-01-28 15:00:48 +0530

    Add files via upload

---
+++ take.py
@@ -0,0 +1,13 @@
+     #take some thing return nothing
+def even():
+     print("enter number")
+     x=eval(input())
+     if x%2==0:
+        print("the number is even")
+     else:
+        print("the nuber is odd") 
+even()    
+ #take something return nothing   
+
+  
+  
